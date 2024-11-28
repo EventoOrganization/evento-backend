105dee0cf2b5fa366a5ac46ad2d87f04594886d1
DUFOURNET Franck <franckdufournet@hotmail.fr>
2024-11-28 16:34:30 +0700

    fix: use put insteed of patch to test

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -12,7 +12,7 @@ router.get("/getRSVPAndReasons/:eventId", eventController.getRSVPAndReasons);
 router.get("/getUpcomingEvents", eventController.getUpcomingEvents);
 
 // Update events
-router.patch(
+router.put(
   "/updateEvent/:eventId",
   authenticateJWT,
   eventController.updateEventField,
