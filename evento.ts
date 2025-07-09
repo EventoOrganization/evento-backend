58d016a18690936328b5974ac67ed9210ae8d54f
DUFOURNET Franck <franckdufournet@hotmail.fr>
2025-07-09 12:32:38 +0700

    chore: add postbuild to cpx views in dist

--- evento.ts
+++ evento.ts
@@ -64,7 +64,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 app.use(express.static(path.join(__dirname, "public")));
-
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(expressLayouts);
